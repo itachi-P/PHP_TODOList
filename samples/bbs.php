d1e6ac1c0921564da84123b8d0a3ccda38c9657c
shinohara itachi <itachip38@gmail.com>
2019-02-25 16:02:03 +0900

    a

---
+++ samples/bbs.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>paiza掲示板</title>
+</head>
+
+<body>
+	<h1>paiza掲示板</h1>
+
+	<h2>投稿フォーム</h2>
+	<p>ここに投稿フォームを追加</p>
+
+	<h2>発言リスト</h2>
+
+	<table>
+		<tr>
+			<th>id</th>
+			<th>日時</th>
+			<th>投稿内容</th>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2016-10-14</td>
+			<td>こんにちは</td>
+		</tr>
+	</table>
+</body>
+
+</html>
