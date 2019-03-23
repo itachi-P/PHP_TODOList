d334adc2c931971f6ff119bd34301028e807a710
shinohara itachi <itachip38@gmail.com>
2019-03-23 10:42:17 +0900

    見た目改善・表記やリンク先修正

--- php/footer.tmp.php
+++ php/footer.tmp.php
@@ -1,11 +1,11 @@
 <footer style='position:absolute; bottom:0; width:100%;'>
 	<p style='background-image: url("../images/ferre_line.gif")'>　</p>
 	<nav>
-	<a href="../index.html">トップページ</a> 
+	<a href="../index.html">トップページ</a>
 	</nav>
 	<small>&copy; Copyright 篠原いたち All Rights Reserved.</small>
 	<address style="display: inline;">
 		<a style="display: inline;">itachip38@gmail.com</a>
 	</address>
-	<div style="float:right">Last Modified: 2019/03/16(Sat) 1:30</div>
+	<div style="float:right">Last Modified: 2019/03/22(Fri) 11:55</div>
 </footer>
--- php/login.php
+++ php/login.php
@@ -18,6 +18,7 @@ require_once("header.tmp.php");
 ?>
 
 <body>
+<!--
 	<span style="color:#77a; font-size: 18px">Ver.2 改修予定</span>
 	<ol style="list-style-type: cjk-ideographic">
 		<li>ユーザー登録・削除機能</li>
@@ -26,7 +27,8 @@ require_once("header.tmp.php");
 			パスワードのハッシュ化、バリデーション(orサニタイズ)チェック強化
 	</ol>
 	<hr>
-	<h1 id="title">ログイン画面</h1>
+-->
+	<h1 id="title">ToDoリスト_ログイン</h1>
 	<div class="login-form">
 		<form method="post" action="./login_check.php">
 			<h2>ログイン</h2>
